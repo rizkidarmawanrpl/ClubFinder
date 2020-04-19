b43d2fd7e14c47638c9b83e00e8f0b784f36045d
erdeprof <rizkidarmawanrpl@gmail.com>
2020-04-19 17:58:24 +0700

    Commit update webpack

--- app.module.js
+++
@@ -1,4 +0,0 @@
-import "./src/script/component/app-bar.js";
-import main from "./src/script/view/main.module.js";
-
-document.addEventListener("DOMContentLoaded", main);
---
+++ src/app.module.js
@@ -0,0 +1,6 @@
+import "regenerator-runtime";
+import "./styles/style.css";
+import "./script/component/app-bar.js";
+import main from "./script/view/main.module.js";
+
+document.addEventListener("DOMContentLoaded", main);
