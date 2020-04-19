9645845cb046192ac6ff000e89eab67475316173
erdeprof <rizkidarmawanrpl@gmail.com>
2020-04-19 10:13:47 +0700

    Commit update shadow DOM

--- src/script/component/club-item.js
+++ src/script/component/club-item.js
@@ -1,4 +1,9 @@
 class ClubItem extends HTMLElement {
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+
     set club(club) {
         this._club = club;
         this.render();
@@ -6,7 +11,47 @@ class ClubItem extends HTMLElement {
 
     render() {
         const data = this._club;
-        this.innerHTML = `
+        this.shadowDOM.innerHTML = `
+        <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        
+        :host {
+            display: block;
+            margin-bottom: 18px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            border-radius: 10px;
+            overflow: hidden;
+        }
+        
+        .fan-art-club {
+            width: 100%;
+            max-height: 300px;
+            object-fit: cover;
+            object-position: center;
+        }
+        
+        .club-info {
+            padding: 24px;
+        }
+        
+        .club-info > h2 {
+            font-weight: lighter;
+        }
+        
+        .club-info > p {
+            margin-top: 10px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 10; /* number of lines to show */
+        }
+        </style>
+
         <img class="fan-art-club" src="${data.fanArt}" alt="Fan Art">
         <div class="club-info">
             <h2>${data.name}</h2>
